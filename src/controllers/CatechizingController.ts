18fdf468ea10d28f3aa86a9a880a2f6173c2c608
Pedro <pedroabernardes11@gmail.com>
2024-08-26 23:47:11 -0300

    refactor: change property name

--- src/controllers/CatechizingController.ts
+++ src/controllers/CatechizingController.ts
@@ -11,7 +11,7 @@ export class CatechizingController {
       const newCatechizingBodySchema = z.object({
         name: z.string(),
         birthday: z.coerce.date(),
-        adress: z.string(),
+        address: z.string(),
         personWithSpecialNeeds: z.boolean(),
         hasReceivedBaptism: z.boolean(),
         hasReceivedEucharist: z.boolean(),
@@ -25,7 +25,7 @@ export class CatechizingController {
 
       const {
         name,
-        adress,
+        address,
         birthday,
         personWithSpecialNeeds,
         hasReceivedBaptism,
@@ -37,7 +37,7 @@ export class CatechizingController {
       const { id } = await prisma.catechizing.create({
         data: {
           name,
-          adress,
+          address,
           birthday,
           hasReceivedBaptism,
           hasReceivedEucharist,
