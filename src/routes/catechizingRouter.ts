fcbb21a3e1bb478a4a29f6f2d213bbc0cd174732
Pedro Antunes <pedro@ctir.gov.br>
2024-08-29 15:32:20 -0300

    refactor: change route param

--- src/routes/catechizingRouter.ts
+++ src/routes/catechizingRouter.ts
@@ -4,7 +4,7 @@ import { CatechizingController } from '../controllers/CatechizingController'
 export async function catechizingRoutes(app: FastifyInstance) {
   app.post('/catechizings', CatechizingController.createNewCatechizing)
   app.get(
-    '/catechizings/:classroomName',
+    '/catechizings/:classroomId',
     CatechizingController.getCatechizingByClassroom,
   )
   app.put(
