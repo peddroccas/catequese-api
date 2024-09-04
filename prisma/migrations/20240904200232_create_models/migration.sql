ac2d47aea7770c8763cd0cd90d0c48a80bcb4838
Pedro Antunes <pedro@ctir.gov.br>
2024-09-04 17:02:40 -0300

    chore: fix migrations

--- prisma/migrations/20240904200232_create_models/migration.sql
+++ prisma/migrations/20240904200232_create_models/migration.sql
@@ -4,14 +4,14 @@ CREATE TABLE "catechists" (
     "name" TEXT NOT NULL,
     "birthday" TIMESTAMP(3) NOT NULL,
     "phone" TEXT NOT NULL,
-    "adress" TEXT NOT NULL,
-    "receivedBaptism" BOOLEAN NOT NULL,
-    "receivedEucharist" BOOLEAN NOT NULL,
-    "receivedConfirmation" BOOLEAN NOT NULL,
-    "receivedMarriage" BOOLEAN,
+    "address" TEXT NOT NULL,
+    "hasReceivedBaptism" BOOLEAN NOT NULL,
+    "hasReceivedEucharist" BOOLEAN NOT NULL,
+    "hasReceivedConfirmation" BOOLEAN NOT NULL,
+    "hasReceivedMarriage" BOOLEAN,
     "email" TEXT,
     "password_hash" TEXT,
-    "classroom_id" TEXT,
+    "classroomId" TEXT,
 
     CONSTRAINT "catechists_pkey" PRIMARY KEY ("id")
 );
@@ -22,12 +22,12 @@ CREATE TABLE "catechizings" (
     "name" TEXT NOT NULL,
     "birthday" TIMESTAMP(3) NOT NULL,
     "age" DECIMAL(65,30),
-    "adress" TEXT NOT NULL,
-    "personWithSpecialNeeds" TEXT,
+    "address" TEXT NOT NULL,
+    "personWithSpecialNeeds" BOOLEAN,
     "hasReceivedBaptism" BOOLEAN NOT NULL,
     "hasReceivedEucharist" BOOLEAN NOT NULL,
     "hasReceivedMarriage" BOOLEAN NOT NULL DEFAULT false,
-    "classroom_id" TEXT,
+    "classroomId" TEXT,
 
     CONSTRAINT "catechizings_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +38,7 @@ CREATE TABLE "parents" (
     "name" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "kinship" TEXT NOT NULL,
-    "catechizing_id" TEXT NOT NULL,
+    "catechizingId" TEXT NOT NULL,
 
     CONSTRAINT "parents_pkey" PRIMARY KEY ("id")
 );
@@ -57,8 +57,8 @@ CREATE TABLE "classrooms" (
 CREATE TABLE "payments" (
     "id" TEXT NOT NULL,
     "toBePaid" DOUBLE PRECISION NOT NULL DEFAULT 150,
-    "hasReceivedTickeT" BOOLEAN NOT NULL DEFAULT false,
-    "catechizing_id" TEXT NOT NULL,
+    "hasReceivedTicket" BOOLEAN NOT NULL DEFAULT false,
+    "catechizingId" TEXT NOT NULL,
 
     CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
 );
@@ -68,7 +68,7 @@ CREATE TABLE "installments" (
     "id" TEXT NOT NULL,
     "payedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "value" DOUBLE PRECISION NOT NULL,
-    "payment_id" TEXT NOT NULL,
+    "paymentId" TEXT NOT NULL,
 
     CONSTRAINT "installments_pkey" PRIMARY KEY ("id")
 );
@@ -80,19 +80,19 @@ CREATE UNIQUE INDEX "catechists_email_key" ON "catechists"("email");
 CREATE UNIQUE INDEX "classrooms_roomNumber_key" ON "classrooms"("roomNumber");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "payments_catechizing_id_key" ON "payments"("catechizing_id");
+CREATE UNIQUE INDEX "payments_catechizingId_key" ON "payments"("catechizingId");
 
 -- AddForeignKey
-ALTER TABLE "catechists" ADD CONSTRAINT "catechists_classroom_id_fkey" FOREIGN KEY ("classroom_id") REFERENCES "classrooms"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "catechists" ADD CONSTRAINT "catechists_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "classrooms"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "catechizings" ADD CONSTRAINT "catechizings_classroom_id_fkey" FOREIGN KEY ("classroom_id") REFERENCES "classrooms"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "catechizings" ADD CONSTRAINT "catechizings_classroomId_fkey" FOREIGN KEY ("classroomId") REFERENCES "classrooms"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "parents" ADD CONSTRAINT "parents_catechizing_id_fkey" FOREIGN KEY ("catechizing_id") REFERENCES "catechizings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "parents" ADD CONSTRAINT "parents_catechizingId_fkey" FOREIGN KEY ("catechizingId") REFERENCES "catechizings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_catechizing_id_fkey" FOREIGN KEY ("catechizing_id") REFERENCES "catechizings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "payments" ADD CONSTRAINT "payments_catechizingId_fkey" FOREIGN KEY ("catechizingId") REFERENCES "catechizings"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "installments" ADD CONSTRAINT "installments_payment_id_fkey" FOREIGN KEY ("payment_id") REFERENCES "payments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "installments" ADD CONSTRAINT "installments_paymentId_fkey" FOREIGN KEY ("paymentId") REFERENCES "payments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
