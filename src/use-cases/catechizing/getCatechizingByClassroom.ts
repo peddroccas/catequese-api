db4448d9615c7d3e6f8010d66b7c76f5db3ad32d
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-01 13:05:16 -0300

    fix: return parent format

--- src/use-cases/catechizing/getCatechizingByClassroom.ts
+++ src/use-cases/catechizing/getCatechizingByClassroom.ts
@@ -7,28 +7,22 @@ interface GetCatechizingByClassroomRequest {
 export async function getCatechizingByClassroom({
   classroomId,
 }: GetCatechizingByClassroomRequest) {
-  const catechizings = await prisma.catechizing
-    .findMany({
-      where: { classroom: { id: classroomId } },
-      select: {
-        id: true,
-        name: true,
-        address: true,
-        birthday: true,
-        classroomId: true,
-        hasReceivedBaptism: true,
-        hasReceivedEucharist: true,
-        hasReceivedMarriage: true,
-        payments: true,
-        parents: { select: { name: true, phone: true, kinship: true } },
-      },
-      orderBy: { name: 'asc' },
-    })
-    .then((catechizings) =>
-      catechizings.map((catechizing) => {
-        return { ...catechizing, parents: catechizing.parents[0] }
-      }),
-    )
+  const catechizings = await prisma.catechizing.findMany({
+    where: { classroom: { id: classroomId } },
+    select: {
+      id: true,
+      name: true,
+      address: true,
+      birthday: true,
+      classroomId: true,
+      hasReceivedBaptism: true,
+      hasReceivedEucharist: true,
+      hasReceivedMarriage: true,
+      payments: true,
+      parents: { select: { name: true, phone: true, kinship: true } },
+    },
+    orderBy: { name: 'asc' },
+  })
 
   return { catechizings }
 }
